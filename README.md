3f0fc6680f055c564978ec5060d63049f55bd1fc
Sean Wilson <hello@imsean.me>
2017-05-21 00:03:10 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# mcsellverify
+
+> MCSell discord verification bot
+
+## Installation
+
+```sh
+$ git clone
+$ npm install
+$ npm start
+```
+
+## Usage
+
+```sh
+!verify   [<profile URL, ex: https://mcsell.org/members/sean.22/> | <verification code: ex: Sk2Apq0lW] 
+```
+
+## License
+
+MIT © [Sean Wilson](https://imsean.me)
---
+++ index.js
@@ -0,0 +1,65 @@
+const cheerio = require('cheerio')
+const request = require('cloudscraper')
+const { Client } = require('discord.js')
+const url = require('url')
+const shortid = require('shortid')
+const config = require('./config.json')
+
+const bot = new Client()
+const cache = []
+
+bot.on('ready', () => console.log('Ready'))
+bot.on('message', message => {
+  const args = message.cleanContent.trim().split(' ')
+  const name = args.shift()
+  const rawURL = args.join(' ')
+
+  if (name === '!verify') {
+    const pendingUser = cache.find(u => u.id === message.author.id)
+
+    const parsedURL = url.parse(rawURL)
+    if (parsedURL.hostname !== 'mcsell.org' && !shortid.isValid(rawURL)) {
+      message.reply('Please provide a link to your MCSell profile (ex. <https://mcsell.org/members/sean.22/>)')
+    } else if (shortid.isValid(rawURL) && pendingUser) {
+      message.reply('Verifying...').then(_message => {
+        request.get(pendingUser.url, (err, response, body) => {
+          if (err) {
+            console.log(err)
+            return _message.edit('An error occurred')
+          }
+
+          const $ = cheerio.load(body)
+          const post = $('#UserStatus').text().split(' ').shift()
+
+          if (pendingUser.code === post.trim()) {
+            const banners = $('.userBanners .userBanner')
+            banners.each(function () {
+              const banner = $(this)
+              const role = config.roleMap[banner.find('b').text().trim()]
+              if (role) {
+                message.member.addRole(role).catch(console.log)
+              }
+            })
+            _message.edit('Verified!')
+          }
+        })
+      })
+    } else if (pendingUser && !shortid.isValid(rawURL)) {
+      message.reply(`You already have a pending verification request, your code is ${pendingUser.code}`)
+    } else if (!pendingUser && shortid.isValid(rawURL)) {
+      message.reply('Please provide a link to your MCSell profile (ex. <https://mcsell.org/members/sean.22/>)')
+    } else {
+      const code = shortid.generate()
+      message.reply(`Your verification code is ${code}, please update your profile status on MCSell with it`)
+      .then(() => {
+        cache.push({
+          id: message.author.id,
+          url: parsedURL.href,
+          code
+        })
+      })
+    }
+  }
+})
+
+bot.login(config.token)
