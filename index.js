1c9c94f79150031c9d0356d1c828687ccd4a6f93
Sean Wilson <hello@imsean.me>
2017-05-26 21:47:45 -0500

    add persistent storage, and mcsell command

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const { Client } = require('discord.js')
 const url = require('url')
 const shortid = require('shortid')
 const config = require('./config.json')
+const low = require('lowdb')
+const db = low('db.json')
+
+db.defaults({ users: [] }).write()
 
 const bot = new Client()
-const cache = []
 
 bot.on('ready', () => console.log('Ready'))
 bot.on('message', message => {
@@ -15,11 +18,13 @@ bot.on('message', message => {
   const rawURL = args.join(' ')
 
   if (name === '!verify') {
-    const pendingUser = cache.find(u => u.id === message.author.id)
+    const pendingUser = db.get('users').find({ id: message.author.id }).value()
 
     const parsedURL = url.parse(rawURL)
     if (parsedURL.hostname !== 'mcsell.org' && !shortid.isValid(rawURL)) {
       message.reply('Please provide a link to your MCSell profile (ex. <https://mcsell.org/members/sean.22/>)')
+    } else if (pendingUser.verified) {
+      message.reply('You are already verified.')
     } else if (shortid.isValid(rawURL) && pendingUser) {
       message.reply('Verifying...').then(_message => {
         request.get(pendingUser.url, (err, response, body) => {
@@ -41,7 +46,9 @@ bot.on('message', message => {
               }
             })
             _message.edit('Verified!').then(() => {
-              cache.forEach((user, index) => user.id === message.author.id ? cache.splice(index, 1) : null)
+              db.get('users').find({ id: message.author.id })
+              .assign({ verified: true })
+              .write()
             })
           }
         })
@@ -52,14 +59,27 @@ bot.on('message', message => {
       message.reply('Please provide a link to your MCSell profile (ex. <https://mcsell.org/members/sean.22/>)')
     } else {
       const code = shortid.generate()
-      message.reply(`Your verification code is ${code}, please update your profile status on MCSell with it`)
+      message.reply(`Your verification code is ${code}, please update your profile status on MCSell with it, once you have done that please type !verify <code>`)
       .then(() => {
-        cache.push({
+        db.get('users').push({
           id: message.author.id,
           url: parsedURL.href,
           code
-        })
+        }).write()
       })
+    }
+  }
+
+  if (name === '!mcsell') {
+    const user = message.mentions.members.array().length ?
+      message.mentions.members.first() :
+      message.member
+
+    const query = db.get('users').find({ id: user.id }).value()
+    if (query) {
+      message.channel.send(`${message.member.user.username}'s mcsell profile is: ${query.url}`)
+    } else {
+      message.reply('Sorry, I couldn\'t find that user. Maybe they haven\'t verified themselves yet.')
     }
   }
 })
